337c69d05171be11037d034e2709f6023ca0052a
ldx86-22 <101376762+ldx86-22@users.noreply.github.com>
2022-03-21 10:29:33 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-   
-   <img class="small">
+<img src="1.jpg"/>
+
+let img = document.getElementsByTagName(`img`)
+img.addEventListener("click",function(){
+img.classList.remove("small")
+img.classList.add("big")}
+                                        
    
    
    
