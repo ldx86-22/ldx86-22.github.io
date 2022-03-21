4fed7df370a0e7ef1cf7b231d9b128502155ec7b
ldx86-22 <101376762+ldx86-22@users.noreply.github.com>
2022-03-21 16:44:29 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     targetImage.addEventListener("click", function() {
     
 });
-    targetImage.classList.add("small");
+   
 targetImage.classList.remove("small");
 
 });
