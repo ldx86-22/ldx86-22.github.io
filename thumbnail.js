ac34c4e00e10b85b40161c965d3eee6995c0edbc
ldx86-22 <101376762+ldx86-22@users.noreply.github.com>
2022-03-21 16:32:33 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     targetImage.addEventListener("click", function() {
-    alert("点击了图片");
+    
 });
     targetImage.classList.add("small");
 targetImage.classList.remove("small");
