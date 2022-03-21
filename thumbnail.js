7c6c6c501e2294cde3691f899b00ecea297448b3
ldx86-22 <101376762+ldx86-22@users.noreply.github.com>
2022-03-21 10:33:37 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,4 @@
-<img src="1.jpg"/>
+ <img src="https://img1.baidu.com/it/u=1212595267,1586342188&fm=253&fmt=auto&app=120&f=PNG?w=716&h=500" alt="图片"/>
 
 let img = document.getElementsByTagName(`img`)
 img.addEventListener("click",function(){
