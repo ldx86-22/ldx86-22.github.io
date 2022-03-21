4770e6ab8920c070a85f6831902525ca11c9f13e
ldx86-22 <101376762+ldx86-22@users.noreply.github.com>
2022-03-21 11:12:52 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,16 +1,7 @@
- <img src="https://img1.baidu.com/it/u=1212595267,1586342188&fm=253&fmt=auto&app=120&f=PNG?w=716&h=500" alt="图片" class="small"/>
-
-let img = document.getElementsByTagName(`img`)
-img.addEventListener("click",function(){
-img.classList.remove("small")
-img.classList.add("big")}
-      targetImage.addEventListener("click", function() {
-    alert("点击了图片");
-});               
-  targetImage.classList.add("small");
-targetImage.classList.remove("small");                         
-                     
-   
-   
+document.addEventListener("DOMContentLoaded", function(event){let.targetImage = document.querySelector("#smart-iamge");
+targetImage.addEventListener("click",function(){alert("点击了图片");
+targetImage.classList.add("small");
+targetImage.classList.remove("small");});
+});
    
 });
