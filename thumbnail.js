c26e388612f6c321b2a79327027eea0c5230f7fb
ldx86-22 <101376762+ldx86-22@users.noreply.github.com>
2022-03-21 17:07:47 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+ let targetImage = document.querySelector("#smart-image");
    targetImage.addEventListener("click", function() {
-  targetImage.classList.remove("small");
+    targetImage.classList.remove("small");
+});
 });  
 });
     
