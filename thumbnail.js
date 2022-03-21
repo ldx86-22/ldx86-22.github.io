5ca200380c723a2b830301c490c0323a8dd374d1
ldx86-22 <101376762+ldx86-22@users.noreply.github.com>
2022-03-21 16:25:57 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  let targetImage = document.querySelector("#smart-image"); 
-  
-targetImage.classList.add("small");
+    targetImage.classList.add("small");
 targetImage.classList.remove("small");
-  
+
 });
