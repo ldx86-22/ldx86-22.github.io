ed7880a813d271cefc82cd464b8455b3c952e95a
ldx86-22 <101376762+ldx86-22@users.noreply.github.com>
2022-03-21 11:17:24 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event){let.targetImage = document.querySelector("#smart-iamge");
+<img src="1.jpg"/ class="small">
+                                                              
 targetImage.addEventListener("click",function(){alert("点击了图片");
 targetImage.classList.add("small");
 targetImage.classList.remove("small");});
